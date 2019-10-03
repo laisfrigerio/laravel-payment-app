51f3cf7ad478ab411b3ac7d1ee9d339e1250db0a
Lais Frigério <laisfrigerio@hotmail.com>
2019-10-03 06:05:14 -0300

    Custom Http request
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- app/Traits/CustomHttpRequest.php
+++ app/Traits/CustomHttpRequest.php
@@ -1,7 +1,48 @@
 <?php
 
 namespace App\Traits;
-class CustomHttpRequest
-{
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+trait CustomHttpRequest
+{
+    public function makeRequest(
+        string $url,
+        string $method = "GET",
+        array $params = [],
+        array $body = [],
+        array $headers = [],
+        bool $isJson = false
+    ) {
+        try {
+            $client = new Client([
+                'base_uri' => $this->base_uri,
+            ]);
+
+            if (method_exists($this, 'resolveAuthorization')) {
+                $this->resolveAuthorization($params, $body, $headers);
+            }
+
+            $response = $client->request($method, $url,  [
+                $isJson ? "json" : "form_params" => $body,
+                "headers" => $headers,
+                "query"=> $params
+            ]);
+
+            $response = $response->getBody()->getContents();
+
+            if (method_exists($this, 'decodeResponse')) {
+                $response = $this->decodeResponse($response);
+            }
+
+            return $response;
+        } catch (\Exception $e) {
+            //
+        } catch (GuzzleException $e) {
+            //
+        }
+
+        return false;
+    }
 }
